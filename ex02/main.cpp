8455bbb7f539d8e8194450cf307c4dd09645db1b
Mohamed Elhadouchi <melhadouchi387@gmail.com>
2023-10-27 13:01:55 +0100

    Done: ex02: add some twiks

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -1,15 +1,16 @@
 #include <iostream>
 
 int main(void) {
-  //
-  //
   std::string var = "HI THIS IS BRAIN";
-  std::string *stringPTR = &var;
-  std::string &stringREF = var;
+  std::string *stringPTR = &var; // setting the	ptr
+  std::string &stringREF = var;  // setting the ref
 
   std::cout << "var => " << &var << std::endl;
   std::cout << "stringPTR => " << &stringPTR << std::endl;
   std::cout << "stringREF => " << &stringREF << std::endl;
+
+  std::cout << "------------------------------------------------" << std::endl;
+
   std::cout << "var value => " << var << std::endl;
   std::cout << "stringPTR value => " << *stringPTR << std::endl;
   std::cout << "stringREF value => " << stringREF << std::endl;
